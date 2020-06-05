c10fd2ad91cb23b0ea29bd3d614ace24023beb35
ShacoCR <66040629+ShacoCR@users.noreply.github.com>
2020-06-05 00:34:39 -0600

    se modifica link

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 (function eyes(){
 let balls = document.querySelector(".ball");
 document.onmousemove = function(){
-  let x = event.clientX * 100 / window.innerWidth + "%";
-  let y = event.clientY * 100 / window.innerHeight + "%";
+  let x = event.clientX * 100 / window.innerWidth + "%"; // Get the horizontal coordinate
+  let y = event.clientY * 100 / window.innerHeight + "%"; // Get the horizontal coordinate
     balls.style.left = x;
     balls.style.top = y;
     balls.style.transform = "translate(-"+x+",-"+y+")";
-  
+  console.log(event.clientX)
 }})();
 
   (function text(){
